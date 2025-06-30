c48c17f498b0a173922908e0b05d0481ca2f59aa
stratisluc (El) <eleassarf@gmail.com>
2025-06-30 02:45:38 -0400

    Update NotFound.jsx
    added shadow to button

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -10,8 +10,8 @@ function NotFound() {
       <div className="text-white flex flex-col justify-center items-center h-screen">
         <h1 className="text-6xl font-rubik">Page Not Found!</h1>
         <Link to={"/"}>
-          <button className="mt-4 bg-white px-4 py-2 rounded-md transition-transform transform hover:scale-110 active:bg-transparent">
-            <span className="font-bungee text-5xl bg-gradient-to-br from-pink-500 to-orange-500 bg-clip-text text-transparent inline-block active:text-white cursor-pointer">
+          <button className="mt-4 bg-white px-4 py-2 rounded-md transition-transform transform hover:scale-110 active:bg-transparent shadow-2xl">
+            <span className="font-bungee text-5xl bg-gradient-to-br from-pink-500 to-orange-500 bg-clip-text text-transparent inline-block active:text-white cursor-pointer ">
               Go Back Home
             </span>
           </button>
