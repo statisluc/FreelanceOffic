f434c15bc6bfa6ef0d9a2831248e779ec547d25e
stratisluc (El) <eleassarf@gmail.com>
2025-07-06 16:41:19 -0400

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,19 +1,27 @@
 import Hero from "./Hero.jsx";
 import Home from "./Home.jsx";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar.jsx";
 
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function Landing() {
+  const [vh, setVh] = useState(() => window.innerHeight);
+
+  useEffect(() => {
+    const onResize = () => setVh(window.innerHeight);
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
   return (
     <div>
-      <Parallax className="w-full h-screen " pages={8}>
+      <Parallax key={vh} className="w-full h-screen " pages={8}>
         <ParallaxLayer
-          className="relative w-full h-screen flex flex-col px-4 pt-40"
+          className="relative w-full h-screen flex flex-col px-4 pt-80 z-20"
           offset={0}
           speed={2}
-          sticky={{ start: 0, end: 5 }}
+          sticky={{ start: 0, end: 3 }}
         >
           <h1 className="text-9xl font-rubik text-white">
             Marketing Yourself...
@@ -21,12 +29,16 @@ function Landing() {
 
           <div className="absolute flex justify-center text-white bottom-32 inset-x-0 breathe">
             <svg
-              className="w-16 h-16"
+              className="w-16 h-16 cursor-pointer"
               fill="none"
               stroke="currentColor"
               strokeWidth="2"
               viewBox="0 0 24 24"
-              onClick={{}}
+              onClick={() =>
+                document
+                  .getElementById("yourwebsite")
+                  .scrollIntoView({ behavior: "smooth" })
+              }
             >
               <path
                 strokeLineCap="round"
@@ -38,10 +50,10 @@ function Landing() {
         </ParallaxLayer>
 
         <ParallaxLayer
-          className="pt-77 w-full h-screen flex justify-end px-4 text-right"
-          offset={1}
+          className="pt-115 w-full h-screen flex justify-end px-4 text-right z-20"
+          offset={0}
           speed={0}
-          sticky={{ start: 3, end: 5 }}
+          sticky={{ start: 2, end: 3 }}
         >
           <h1 className="text-8xl font-bungee text-white">
             ...Just Got A Whole Lot Easier.
@@ -53,10 +65,20 @@ function Landing() {
           </ParallaxLayer> */}
         </div>
         <ParallaxLayer
-          className="flex justify-center items-center h-screen"
+          className="flex justify-center items-center h-screen z-1"
           offset={2}
+          sticky={{ start: 4, end: 6 }}
         >
-          <img src="yourcodehere.png" alt="Screenshot of Website Code!"></img>
+          <img
+            src="yourcodehere.png"
+            alt="Screenshot of Website Code!"
+            className="h-auto w-[70vw]"
+            id="yourwebsite"
+          ></img>
+        </ParallaxLayer>
+
+        <ParallaxLayer>
+          <h1>This Could Be Your Website!</h1>
         </ParallaxLayer>
       </Parallax>
     </div>
