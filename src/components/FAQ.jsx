4f9efc9edf0d3cb7f5990923883bb6bcf707f1bb
Alex <eleassarf@gmail.com>
2025-07-14 15:10:16 -0400

    Update FAQ.jsx
    creating faq

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -14,3 +14,12 @@ function FAQ() {
 }
 
 export default FAQ;
+
+// 1) What do you charge based on?
+// I charge based on time, effort, and quality put into the project. Please see my pricing page HERE_link.
+// 2) What if I want my website to look a specific way?
+// I have video conference consultation calls where we can discuss the specifics of the website design and layout. These are
+// $15 per hour, with a $5 down payment to start the meeting. Upon receiving this payment, we can schedule the minutes or hours of the meeting
+// and how much the total cost will be (subtract the $5 down payment). Failure to make the $5 deposit will result in no appointment being reached, and failure to pay for the meeting will result in termination of our partnership.
+// 3) I found a bug in the website! What can I do?
+// Bugs are common and part of the software development cycle. For every website I make, I offer a 2 week warranty from date of deploymnet. This is your opportunity to "break" the website and report to me your findings. Any bug found after these two weeks can be resolved by me for $30, or $25 per hour of work, whichever greater.
